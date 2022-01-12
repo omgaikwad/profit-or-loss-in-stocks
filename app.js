68e0dff7da98e816bc5f1e8f7eb8a8a6f8932a58
Om Gaikwad <62072438+omgaikwad@users.noreply.github.com>
2022-01-12 22:41:25 +0530

    updated code to ES6+

--- app.js
+++ app.js
@@ -4,41 +4,36 @@ const currentPrice = document.querySelector("#current-price");
 const checkBtn = document.querySelector("#check-btn");
 const outputDiv = document.querySelector("#output-div");
 
-function clickHandler(){
-    var ip = Number(initialPrice.value);
-    var qty = Number(quantityOfStocks.value);
-    var curr = Number(currentPrice.value);
+const clickHandler = () => {
+  let ip = Number(initialPrice.value);
+  let qty = Number(quantityOfStocks.value);
+  let curr = Number(currentPrice.value);
 
-    if(initialPrice.value && quantityOfStocks.value && currentPrice.value){
-        if(curr > ip){
-            // Profit
-            var profitPerStock = curr - ip;
-            var totalProfit = profitPerStock * qty;
-            var profitPercent = (profitPerStock/ip) * 100;
-    
-            outputDiv.style.color = "green";
-            outputDiv.innerText = `Your Total Profit is Rs. ${totalProfit}. Your Profit Percent is ${profitPercent}%`;
-        }
-        else if(ip > curr){
-            // Loss
-            var lossPerStock = ip - curr;
-            var totalLoss = lossPerStock * qty;
-            var lossPercent = (lossPerStock/ip) * 100;
-    
-            outputDiv.style.color = "red";
-            outputDiv.innerText = `Your Total Loss is Rs. ${totalLoss}. Your Loss Percent is ${lossPercent}%`;
-        }
-        else{
-            // No Gain No Pain
-            outputDiv.style.color = "yellow";
-            outputDiv.innerText = `No Profit, No Loss`;
-        }
-    }
-    else{
-        outputDiv.innerHTML = "😡Enter all the fields!"
-    }
+  if (initialPrice.value && quantityOfStocks.value && currentPrice.value) {
+    if (curr > ip) {
+      // Profit
+      let profitPerStock = curr - ip;
+      let totalProfit = profitPerStock * qty;
+      let profitPercent = (profitPerStock / ip) * 100;
 
-    
-}
+      outputDiv.style.color = "green";
+      outputDiv.innerText = `Your Total Profit is Rs. ${totalProfit}. Your Profit Percent is ${profitPercent}%`;
+    } else if (ip > curr) {
+      // Loss
+      let lossPerStock = ip - curr;
+      let totalLoss = lossPerStock * qty;
+      let lossPercent = (lossPerStock / ip) * 100;
+
+      outputDiv.style.color = "red";
+      outputDiv.innerText = `Your Total Loss is Rs. ${totalLoss}. Your Loss Percent is ${lossPercent}%`;
+    } else {
+      // No Gain No Pain
+      outputDiv.style.color = "yellow";
+      outputDiv.innerText = `No Profit, No Loss`;
+    }
+  } else {
+    outputDiv.innerHTML = "😡Enter all the fields!";
+  }
+};
 
 checkBtn.addEventListener("click", clickHandler);
